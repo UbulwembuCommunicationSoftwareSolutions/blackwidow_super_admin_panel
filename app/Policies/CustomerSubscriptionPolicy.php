5924cae544bb58c89580fd63898cbe36242ad70d
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-07-11 15:27:10 +0200

    Testingf 123

--- app/Policies/CustomerSubscriptionPolicy.php
+++ app/Policies/CustomerSubscriptionPolicy.php
@@ -12,30 +12,36 @@ class CustomerSubscriptionPolicy
 
     public function viewAny(User $user): bool
     {
-
+       return true;
     }
 
     public function view(User $user, CustomerSubscription $customerSubscription): bool
     {
+       return true;
     }
 
     public function create(User $user): bool
     {
+       return true;
     }
 
     public function update(User $user, CustomerSubscription $customerSubscription): bool
     {
+       return true;
     }
 
     public function delete(User $user, CustomerSubscription $customerSubscription): bool
     {
+       return true;
     }
 
     public function restore(User $user, CustomerSubscription $customerSubscription): bool
     {
+       return true;
     }
 
     public function forceDelete(User $user, CustomerSubscription $customerSubscription): bool
     {
+       return true;
     }
 }
