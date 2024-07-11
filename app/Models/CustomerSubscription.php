9209e775530ba64ceeb517053bea56482fa44087
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-07-11 15:31:29 +0200

    Testingf 123

--- app/Models/CustomerSubscription.php
+++ app/Models/CustomerSubscription.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
-class CustomerSubscription extends Model
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+class CustomerSubscription extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+
+    protected $fillable = [
+        'logo_1',
+        'logo_2',
+        'logo_3',
+        'logo_4',
+        'logo_5',
+    ];
+
     public function subscriptionType(): BelongsTo
     {
         return $this->belongsTo(SubscriptionType::class);
