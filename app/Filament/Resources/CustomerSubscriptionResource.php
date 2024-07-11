184f115abaf27c084a4e5d1dd9bee2dd6cd4fdbd
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-07-11 15:38:25 +0200

    Testingf 123

--- app/Filament/Resources/CustomerSubscriptionResource.php
+++ app/Filament/Resources/CustomerSubscriptionResource.php
@@ -33,15 +33,6 @@ class CustomerSubscriptionResource extends Resource
                     ->label('Customer')
                     ->relationship('customer', 'name') // Specify the relationship and the display column
                     ->required(),
-
-                Placeholder::make('created_at')
-                    ->label('Created Date')
-                    ->content(fn(?CustomerSubscription $record): string => $record?->created_at?->diffForHumans() ?? '-'),
-
-                Placeholder::make('updated_at')
-                    ->label('Last Modified Date')
-                    ->content(fn(?CustomerSubscription $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
-
                 TextInput::make('url')
                     ->required()
                     ->url(),
