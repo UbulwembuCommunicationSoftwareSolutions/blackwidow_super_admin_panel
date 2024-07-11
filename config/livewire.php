34b892cc0f330b8ee7717fb66dc3a969c1bd26a0
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-07-11 15:52:51 +0200

    fdfg

--- config/livewire.php
+++ config/livewire.php
@@ -66,7 +66,7 @@ return [
     'temporary_file_upload' => [
         'disk' => env('filesystem_disk'),        // Example: 'local', 's3'              | Default: 'default'
         'rules' =>  ['file', 'mimes:png,jpg'],       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => 'storage',   // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'directory' => 'storage/uploads',   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
