048445549a533af1b5f828f4ac10259a7c054ae3
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-07-11 15:27:20 +0200

    Testingf 123

--- app/Policies/CustomerSubscriptionPolicy.php
+++ app/Policies/CustomerSubscriptionPolicy.php
@@ -3,7 +3,7 @@
 namespace App\Policies;
 
 use App\Models\CustomerSubscription;
-use AppModels\User;
+use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class CustomerSubscriptionPolicy
