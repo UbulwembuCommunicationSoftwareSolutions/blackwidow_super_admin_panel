846b2c6d08bff0cf8d0f6337aada513b63e3738b
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-07-12 11:36:21 +0200

    fdfg

--- app/Filament/Resources/CustomerResource/RelationManagers/CustomerSubscriptionsRelationManager.php
+++ app/Filament/Resources/CustomerResource/RelationManagers/CustomerSubscriptionsRelationManager.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\CustomerResource\RelationManagers;
 
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
@@ -21,6 +22,45 @@ class CustomerSubscriptionsRelationManager extends RelationManager
                 Forms\Components\TextInput::make('url')
                     ->required()
                     ->maxLength(255),
+                FileUpload::make('logo_1')
+                    ->label('Logo 1')
+                    ->disk('public')
+                    ->visibility('public') // Or 'private' based on your requirements
+                    ->disk('public') // The disk defined in your `config/filesystems.php`
+                    ->nullable()
+                    ->rules(['nullable', 'file', 'max:10240']),
+
+                FileUpload::make('logo_2')
+                    ->label('Logo 2')
+                    ->disk('public')
+                    ->visibility('public') // Or 'private' based on your requirements
+                    ->disk('public') // The disk defined in your `config/filesystems.php`
+                    ->nullable()
+                    ->rules(['nullable', 'file', 'max:10240']),
+
+                FileUpload::make('logo_3')
+                    ->label('Logo 3')
+                    ->disk('public')
+                    ->visibility('public') // Or 'private' based on your requirements
+                    ->disk('public') // The disk defined in your `config/filesystems.php`
+                    ->nullable()
+                    ->rules(['nullable', 'file', 'max:10240']),
+
+                FileUpload::make('logo_4')
+                    ->label('Logo 4')
+                    ->disk('public')
+                    ->visibility('public') // Or 'private' based on your requirements
+                    ->disk('public') // The disk defined in your `config/filesystems.php`
+                    ->nullable()
+                    ->rules(['nullable', 'file', 'max:10240']),
+
+                FileUpload::make('logo_5')
+                    ->label('Logo 5')
+                    ->disk('public')
+                    ->visibility('public') // Or 'private' based on your requirements
+                    ->disk('public') // The disk defined in your `config/filesystems.php`
+                    ->nullable()
+                    ->rules(['nullable', 'file', 'max:10240']),
             ]);
     }
 
