d8f894c1559673361d3798d8d30afeee7c3b34b5
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-08-05 11:58:45 +0200

    fdfg

--- app/Models/EnvVariables.php
+++ app/Models/EnvVariables.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EnvVariables extends Model
 {
+
+    public $fillable = [
+        'key',
+        'value',
+        'customer_subscription_id',
+        'created_at',
+        'updated_at',
+    ];
     public function customerSubscription(): BelongsTo
     {
         return $this->belongsTo(CustomerSubscription::class);
