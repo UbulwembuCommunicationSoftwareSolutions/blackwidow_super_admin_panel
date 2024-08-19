aebb9b388dac28f2e144b4d13962cf46037a098b
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-08-19 12:13:43 +0200

    fdfg

--- app/Models/RequiredEnvVariables.php
+++ app/Models/RequiredEnvVariables.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RequiredEnvVariables extends Model
 {
+
+    public $fillable = [
+        'subscription_type_id',
+        'key',
+        'value',
+        'created_at',
+        'updated_at',
+    ];
     public function subscriptionType(): BelongsTo
     {
         return $this->belongsTo(SubscriptionType::class);
