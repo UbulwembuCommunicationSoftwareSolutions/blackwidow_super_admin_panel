828406af63a70704e400afa5d271cd9826e8c2ba
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-12-12 12:37:18 +0200

    fdfg

--- app/Models/CustomerUser.php
+++ app/Models/CustomerUser.php
@@ -8,6 +8,22 @@ use Laravel\Sanctum\HasApiTokens;
 class CustomerUser extends Model
 {
     use HasApiTokens;
+
+    public $fillable = [
+        'customer_id',
+        'name',
+        'email',
+        'password',
+        'phone',
+        'address',
+        'city',
+        'state',
+        'zip',
+        'country',
+        'lat',
+        'lng',
+        'google_address',
+    ];
     public function customer(): BelongsTo
     {
         return $this->belongsTo(Customer::class);
