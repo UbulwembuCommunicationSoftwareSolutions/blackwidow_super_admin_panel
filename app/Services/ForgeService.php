7e51365a183c2e6b16ed7326b0cb638b1947497b
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-12-17 13:21:36 +0200

    fdfg

--- app/Services/ForgeService.php
+++ app/Services/ForgeService.php
@@ -56,7 +56,8 @@ class ForgeService
                 $deploymentString = str_replace('#WEBSITE_URL#',$baseUrl,$deploymentTemplate->script);
                 if($customerSubscription->server_id && $customerSubscription->forge_site_id){
                     try{
-                        $forgeApi->forge->updateSiteDeploymentScript($customerSubscription->serverId, $customerSubscription->forge_site_id, $deploymentString);
+                        $site = $forgeApi->forge->site($customerSubscription->server_id, $customerSubscription->forge_site_id);
+                        $site->updateSiteDeploymentScript($customerSubscription->serverId, $customerSubscription->forge_site_id, $deploymentString);
                         echo "Success ". $customerSubscription->url.','.$customerSubscription->server_id.','.$customerSubscription->forge_site_id."\n";
 
                     }catch (\Exception $e){
