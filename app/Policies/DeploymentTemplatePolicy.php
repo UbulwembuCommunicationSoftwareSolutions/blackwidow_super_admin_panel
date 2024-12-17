c52ca9871b9f04eb3278ca146081cd970f68bf72
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-12-17 12:44:40 +0200

    fdfg

--- app/Policies/DeploymentTemplatePolicy.php
+++ app/Policies/DeploymentTemplatePolicy.php
@@ -2,40 +2,107 @@
 
 namespace App\Policies;
 
-use App\Models\DeploymentTemplate;
 use App\Models\User;
+use App\Models\DeploymentTemplate;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class DeploymentTemplatePolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * Determine whether the user can view any models.
+     */
     public function viewAny(User $user): bool
     {
-
+        return $user->can('view_any_deployment::template');
     }
 
+    /**
+     * Determine whether the user can view the model.
+     */
     public function view(User $user, DeploymentTemplate $deploymentTemplate): bool
     {
+        return $user->can('view_deployment::template');
     }
 
+    /**
+     * Determine whether the user can create models.
+     */
     public function create(User $user): bool
     {
+        return $user->can('create_deployment::template');
     }
 
+    /**
+     * Determine whether the user can update the model.
+     */
     public function update(User $user, DeploymentTemplate $deploymentTemplate): bool
     {
+        return $user->can('update_deployment::template');
     }
 
+    /**
+     * Determine whether the user can delete the model.
+     */
     public function delete(User $user, DeploymentTemplate $deploymentTemplate): bool
     {
+        return $user->can('delete_deployment::template');
     }
 
-    public function restore(User $user, DeploymentTemplate $deploymentTemplate): bool
+    /**
+     * Determine whether the user can bulk delete.
+     */
+    public function deleteAny(User $user): bool
     {
+        return $user->can('delete_any_deployment::template');
     }
 
+    /**
+     * Determine whether the user can permanently delete.
+     */
     public function forceDelete(User $user, DeploymentTemplate $deploymentTemplate): bool
     {
+        return $user->can('force_delete_deployment::template');
+    }
+
+    /**
+     * Determine whether the user can permanently bulk delete.
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->can('force_delete_any_deployment::template');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     */
+    public function restore(User $user, DeploymentTemplate $deploymentTemplate): bool
+    {
+        return $user->can('restore_deployment::template');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->can('restore_any_deployment::template');
+    }
+
+    /**
+     * Determine whether the user can replicate.
+     */
+    public function replicate(User $user, DeploymentTemplate $deploymentTemplate): bool
+    {
+        return $user->can('replicate_deployment::template');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     */
+    public function reorder(User $user): bool
+    {
+        return $user->can('reorder_deployment::template');
     }
 }
