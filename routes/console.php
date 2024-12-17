86a92a33302e15fc5ec085fbd6ac511c5aa98bde
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-12-17 11:09:20 +0200

    fdfg

--- routes/console.php
+++ routes/console.php
@@ -39,7 +39,7 @@ Artisan::command('app:importExistingUsers',function (){
         $database = $console->envVariables()->where('customer_subscription_id',$console->id)->where('key','DB_DATABASE')->first();
         $user = $console->envVariables()->where('customer_subscription_id',$console->id)->where('key','DB_USERNAME')->first();
         $password = $console->envVariables()->where('customer_subscription_id',$console->id)->where('key','DB_PASSWORD')->first();
-        dd($database->value,$user->value,$password->value);
+        $this->info($database->value,$user->value,$password->value);
     }
 })->purpose('Import existing Case Management Users')->daily();
 //
