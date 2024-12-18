492fab959644edc19d1f7ea47cd93f611024737d
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-12-18 15:57:01 +0200

    fdfg

--- app/Helpers/ForgeApi.php
+++ app/Helpers/ForgeApi.php
@@ -53,7 +53,6 @@ class ForgeApi
 
     public function getSitesForServer($serverId){
         $sites = $this->getSites($serverId);
-        dd($sites);
         foreach ($sites as $site) {
             $customerSubscription = CustomerSubscription::where('url', 'like', '%' . $site->name . '%')->first();
             if ($customerSubscription) {
