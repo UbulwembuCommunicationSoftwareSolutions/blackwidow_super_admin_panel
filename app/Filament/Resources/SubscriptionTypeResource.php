667cc629e4b77ef4e26c9bd3eb0e81f4f8864409
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-12-18 14:58:38 +0200

    fdfg

--- app/Filament/Resources/SubscriptionTypeResource.php
+++ app/Filament/Resources/SubscriptionTypeResource.php
@@ -50,8 +50,6 @@ class SubscriptionTypeResource extends Resource
                     ->required(),
                 TextInput::make('branch')
                     ->required(),
-                Placeholder::make()
-                ->content('The branch name is the name of the branch in the GitHub repository that will be used to deploy the subscription type. The branch must exist in the GitHub repository and be accessible to the GitHub App that is used to deploy the subscription type.'),
             ]);
     }
 
