b3ac4c2dac95261e5de72d4283581db18a459c04
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-12-19 13:57:46 +0200

    fdfg

--- app/Helpers/ForgeApi.php
+++ app/Helpers/ForgeApi.php
@@ -145,12 +145,12 @@ class ForgeApi
         $this->addMissingEnv($customerSubscription);
         $payload = [
             'domain' => $customerSubscription->domain,
-            'project_type' => $customerSubscription->subscriptionType->project_type,
+            'project_type' => 'php/laravel',
             'directory' => '/public',
             'php_version' => 'php83',
             'repository' => $customerSubscription->subscriptionType->github_repo,
-//            'repository_provider' => 'github',
-//            'repository_branch' => $customerSubscription->subscriptionType->branch,
+            'repository_provider' => 'github',
+            'repository_branch' => $customerSubscription->subscriptionType->branch,
             'database' => $customerSubscription->database_name,
 //            'env' => $this->collectEnv($customerSubscription)
         ];
