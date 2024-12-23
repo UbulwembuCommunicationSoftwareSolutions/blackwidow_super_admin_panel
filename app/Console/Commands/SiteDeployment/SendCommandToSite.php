d9b565a4215c41474ce4b6f10a988409ac571d81
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-12-23 14:10:36 +0200

    fdfg

---
+++ app/Console/Commands/SiteDeployment/SendCommandToSite.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands\SiteDeployment;
+
+use Illuminate\Console\Command;
+
+class SendCommandToSite extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-command-to-site {site-id} {command}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        SendCommandToSite::dispatch($this->argument('site-id'), $this->argument('command'));
+        $this->info('Command has been dispatched to all consoles.');
+        return Command::SUCCESS;
+    }
+}
