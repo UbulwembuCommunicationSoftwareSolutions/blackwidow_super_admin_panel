9720ddf4ed66b2aa77b73ed3f1508d4bfa6dd33b
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-12-23 14:12:34 +0200

    fdfg

--- app/Console/Commands/SiteDeployment/SendCommandToSite.php
+++ app/Console/Commands/SiteDeployment/SendCommandToSite.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands\SiteDeployment;
 
+use App\Jobs\SendCommandToForge;
 use Illuminate\Console\Command;
 
 class SendCommandToSite extends Command
@@ -25,7 +26,7 @@ class SendCommandToSite extends Command
      */
     public function handle()
     {
-        SendCommandToSite::dispatch($this->argument('site-id'), $this->argument('command'));
+        SendCommandToForge::dispatch($this->argument('site-id'), $this->argument('command'));
         $this->info('Command has been dispatched to all consoles.');
         return Command::SUCCESS;
     }
