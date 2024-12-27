82bc6206cab8423f4341f6602e6230bbffeeff7f
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-12-27 08:52:00 +0200

    fdfg

---
+++ database/migrations/2024_12_27_065145_add_public_dir_to_subscription_types.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('subscription_types', function (Blueprint $table) {
+            $table->string('public_dir')->default('dist');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('subscription_types', function (Blueprint $table) {
+            $table->dropColumn('public_dir');
+        });
+    }
+};
