6d39368580afa274174b577cd408ee7c7b15fa36
Jacquestrdx123 <jacques.trdx@gmail.com>
2024-12-31 06:41:28 +0200

    fdfg

--- app/Filament/Resources/CustomerSubscriptionResource/Pages/CreateCustomerSubscription.php
+++ app/Filament/Resources/CustomerSubscriptionResource/Pages/CreateCustomerSubscription.php
@@ -266,22 +266,6 @@ class CreateCustomerSubscription extends CreateRecord
 
         if (in_array($this->record->subscription_type_id, [1, 2, 9, 10])) {
             $jobs[] = array(
-                'id' => SendCommandToForgeJob::dispatch($this->record->id,'mkdir storage/framework')->delay(now()->addMinutes(7)),
-                'progress' => 0
-            );
-            $jobs[] = array(
-                'id' => SendCommandToForgeJob::dispatch($this->record->id,'mkdir storage/framework/cache')->delay(now()->addMinutes(7)),
-                'progress' => 0
-            );
-            $jobs[] = array(
-                'id' => SendCommandToForgeJob::dispatch($this->record->id,'mkdir storage/framework/views')->delay(now()->addMinutes(7)),
-                'progress' => 0
-            );
-            $jobs[] = array(
-                'id' => SendCommandToForgeJob::dispatch($this->record->id,'mkdir storage/framework/sessions')->delay(now()->addMinutes(7)),
-                'progress' => 0
-            );
-            $jobs[] = array(
                 'id' => SendCommandToForgeJob::dispatch($this->record->id,'php artisan key:generate --force')->delay(now()->addMinutes(7)),
                 'progress' => 0
             );
