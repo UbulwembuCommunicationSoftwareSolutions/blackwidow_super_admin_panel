657043fc25872f3264362fedfc37123eef7b0b93
Jacquestrdx123 <jacques.trdx@gmail.com>
2025-01-03 10:03:54 +0200

    fdfg

--- app/Services/CMSService.php
+++ app/Services/CMSService.php
@@ -9,6 +9,7 @@ class CMSService
 {
     public function setConsoleSystemConfigs($subscription){
         $url = $subscription->url.'/api/admin-api/admin';
+        echo 'Doing request to '.$url.' with token '.$subscription->customer->token.PHP_EOL;
         $response = Http::withToken($subscription->customer->token)->post($url);
         dd($response->body());
     }
