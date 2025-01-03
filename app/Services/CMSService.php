82d70573c7f71cc7dc2b0fd50b4a0932670070a0
Jacquestrdx123 <jacques.trdx@gmail.com>
2025-01-03 09:59:10 +0200

    fdfg

--- app/Services/CMSService.php
+++ app/Services/CMSService.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Http;
 class CMSService
 {
     public function setConsoleSystemConfigs($subscription){
-        $url = $subscription->url.'/admin-api/admin';
+        $url = $subscription->url.'/api/admin-api/admin';
         $response = Http::withToken($subscription->customer->token)->get($url);
         dd($response->body());
     }
