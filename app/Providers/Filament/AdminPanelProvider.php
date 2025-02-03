f240add0634738f3eecd60d0b79eb4e9e1aadf45
Jacquestrdx123 <jacques.trdx@gmail.com>
2025-02-03 09:57:44 +0200

    fdfg

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -45,6 +45,7 @@ class AdminPanelProvider extends PanelProvider
                 Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
+                EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
                 StartSession::class,
                 AuthenticateSession::class,
