ffddae05b1cdf16bdc57f5de9e0a959dc490691b
Jacquestrdx123 <jacques.trdx@gmail.com>
2025-03-03 12:25:31 +0200

    fdfg

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -3,11 +3,15 @@
 namespace App\Helpers;
 
 use Illuminate\Support\Facades\Storage;
+use Spatie\Image\Exceptions\CouldNotLoadImage;
 use Spatie\Image\Image;
 use Spatie\ImageOptimizer\OptimizerChainFactory;
 
 class ImageHelper
 {
+    /**
+     * @throws CouldNotLoadImage
+     */
     public static function generatePwaIcons($subscription, $imagePath)
     {
         // Define the required PWA icon sizes
@@ -16,22 +20,34 @@ class ImageHelper
         $imagePath = Storage::disk('public')->path($imagePath);
         // Define the storage path for PWA icons
 
+        if (!Storage::exists($imagePath)) {
+            throw new \Exception("Image file not found: " . $imagePath);
+        }
+
         $basePath = Storage::disk('public')->path('pwa-icons/'.$subscription->id);
 
         Storage::makeDirectory($basePath);
 
         foreach ($sizes as $size) {
             $outputPath = "{$basePath}/icon-{$size}x{$size}.png";
+            $storagePath = Storage::path($outputPath);
+            if (!is_writable(dirname($storagePath))) {
+                throw new \Exception("Directory not writable: " . dirname($storagePath));
+            }
 
+            \Log::error("Saving image to: " . Storage::path($outputPath));
             // Resize and save the image
             Image::load($imagePath)
                 ->width($size)
                 ->height($size)
-                ->save(Storage::path($outputPath));
+                ->save(Storage::disk('public')->path($outputPath));
 
             // Optimize the image
             $optimizerChain = OptimizerChainFactory::create();
             $optimizerChain->optimize(Storage::path($outputPath));
+            if (!Storage::exists($outputPath)) {
+                throw new \Exception("Image was not saved.");
+            }
         }
 
         return true;
