7ca8b60d46464a4241b01596f4226958fb225d1e
Jacquestrdx123 <jacques.trdx@gmail.com>
2025-04-02 08:11:35 +0200

    fdfg

--- app/Console/Commands/SiteDeployment/DeploySiteByType.php
+++ app/Console/Commands/SiteDeployment/DeploySiteByType.php
@@ -31,7 +31,7 @@ class DeploySiteByType extends Command
 
         foreach ($customerSubscriptions as $customerSubscription) {
             \App\Jobs\SiteDeployment\DeploySite::dispatch($customerSubscription->id)->delay($delay);
-            $delay = $delay->addSeconds(10); // Increment delay by 1 minute for each job
+            $delay = $delay->addMinute(1); // Increment delay by 1 minute for each job
         }
     }
 }
