336936f04caa7e842c343a5e63e12046d8e235a2
Jacquestrdx123 <jacques.trdx@gmail.com>
2025-07-22 14:03:52 +0200

    Moving Login To SuperAdmin Directly

--- app/Http/Controllers/CustomerUserController.php
+++ app/Http/Controllers/CustomerUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\CustomerUserResource;
+use App\Jobs\StartUserSyncJob;
 use App\Models\Customer;
 use App\Models\CustomerSubscription;
 use App\Models\CustomerUser;
