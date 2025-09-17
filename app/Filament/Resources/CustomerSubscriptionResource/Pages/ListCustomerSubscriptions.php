7f292c03e866ca3fe3694307d266d8224b9cc23c
Jacquestrdx123 <jacques.trdx@gmail.com>
2025-09-17 13:46:23 +0200

    Corporate Updatesush

--- app/Filament/Resources/CustomerSubscriptionResource/Pages/ListCustomerSubscriptions.php
+++ app/Filament/Resources/CustomerSubscriptionResource/Pages/ListCustomerSubscriptions.php
@@ -5,7 +5,8 @@ namespace App\Filament\Resources\CustomerSubscriptionResource\Pages;
 use App\Filament\Resources\CustomerSubscriptionResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
-
+use pxlrbt\FilamentExcel\Actions\Tables\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 class ListCustomerSubscriptions extends ListRecords
 {
     protected static string $resource = CustomerSubscriptionResource::class;
@@ -14,6 +15,7 @@ class ListCustomerSubscriptions extends ListRecords
     {
         return [
             CreateAction::make(),
+            ExcelExport::make('table')->queue()->fromTable(),
         ];
     }
 
