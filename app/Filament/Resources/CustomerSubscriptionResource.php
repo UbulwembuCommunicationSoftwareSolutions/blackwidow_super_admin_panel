7b03f89292e2b49cf6eded36c5f9726e2ea0dbc6
Jacquestrdx123 <jacques.trdx@gmail.com>
2025-09-17 13:50:21 +0200

    Corporate Updatesush

--- app/Filament/Resources/CustomerSubscriptionResource.php
+++ app/Filament/Resources/CustomerSubscriptionResource.php
@@ -18,18 +18,6 @@ use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Tables\Actions\BulkAction;
-use Filament\Tables\Actions\BulkActionGroup;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Columns\ToggleColumn;
-use Filament\Tables\Filters\Filter;
-use Filament\Tables\Filters\SelectFilter;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Collection;
 
 class CustomerSubscriptionResource extends Resource
@@ -83,74 +71,6 @@ class CustomerSubscriptionResource extends Resource
             ]);
     }
 
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                TextColumn::make('url')
-                    ->label('Website')
-                    ->formatStateUsing(fn ($state) => '<a href="' . $state . '" target="_blank" rel="noopener noreferrer">'.$state.'</a>')
-                    ->html()
-                    ->sortable(),
-                TextColumn::make('domain')
-                    ->searchable()
-                    ->sortable(),
-                TextColumn::make('app_name')
-                    ->label('App Name')
-                    ->searchable()
-                    ->sortable(),
-                TextColumn::make('customer.company_name')
-                    ->label('Customer')
-                    ->searchable()
-                    ->sortable(),
-                TextColumn::make('subscriptionType.name')
-                    ->sortable()
-                    ->searchable(),
-                TextColumn::make('deployed_at')
-                    ->label('Deployed Date')
-                    ->dateTime('Y-m-d H:i:s')
-                    ->searchable()
-                    ->sortable(),
-                TextColumn::make('deployed_version')
-                    ->label('Deployed Version')
-                    ->searchable()
-                    ->sortable(),
-                TextColumn::make('subscriptionType.master_version')
-                    ->label('Newest Version')
-                    ->searchable()
-                    ->sortable(),
-                ToggleColumn::make('panic_button_enabled')
-                    ->label('Panic Button')
-                    ->disabled(fn ($record) => !$record || self::isThisAppTypeSubscription($record->subscription_type_id)),
-                TextColumn::make('forge_site_id'),
-                TextColumn::make('env_variables_count')
-                    ->label('Variable Count')
-                    ->counts('envVariables'),
-                TextColumn::make('null_variable_count')
-                    ->label('Null Count')
-            ])
-            ->filters([
-                SelectFilter::make('subscription_type_id')
-                    ->label('Subscription Type')
-                    ->relationship('subscriptionType', 'name') // Assuming 'subscriptionType' is the relationship method name
-                    ->options(SubscriptionType::pluck('name', 'id')),
-            ])
-            ->actions([
-                EditAction::make(),
-                DeleteAction::make(),
-            ])
-            ->bulkActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                    BulkAction::make('send_ent_to_forge')
-                        ->label('Send ENV To Forge')
-                        ->action(fn (Collection $records) => CustomerSubscriptionResource::sendEnvs($records))
-                        ->requiresConfirmation()
-                        ->deselectRecordsAfterCompletion()
-                        ->color('primary'),
-                ]),
-            ]);
-    }
 
     public static function  isThisAppTypeSubscription($subscriptionTypeId): bool
     {
@@ -158,16 +78,6 @@ class CustomerSubscriptionResource extends Resource
         return in_array($subscriptionTypeId, [3, 4, 5, 6, 7]);
     }
 
-    public static function getEloquentQuery(): Builder
-    {
-        return parent::getEloquentQuery()
-            ->with('customer', 'subscriptionType')
-            ->with('envVariables')
-            ->withCount('envVariables')
-            ->withoutGlobalScopes([
-                SoftDeletingScope::class,
-            ]);
-    }
 
     public static function getPages(): array
     {
--- app/Filament/Resources/CustomerSubscriptionResource/Pages/ListCustomerSubscriptions.php
+++ app/Filament/Resources/CustomerSubscriptionResource/Pages/ListCustomerSubscriptions.php
@@ -3,20 +3,109 @@
 namespace App\Filament\Resources\CustomerSubscriptionResource\Pages;
 
 use App\Filament\Resources\CustomerSubscriptionResource;
+use App\Models\SubscriptionType;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Collection;
 use pxlrbt\FilamentExcel\Actions\Tables\ExportAction;
+
 class ListCustomerSubscriptions extends ListRecords
 {
     protected static string $resource = CustomerSubscriptionResource::class;
 
-    protected function getHeaderActions(): array
+
+    public function table(Table $table): Table
     {
-        return [
-            CreateAction::make(),
-            ExportAction::make('table')->fromTable(),
-        ];
+        return $table
+            ->columns([
+                TextColumn::make('url')
+                    ->label('Website')
+                    ->formatStateUsing(fn ($state) => '<a href="' . $state . '" target="_blank" rel="noopener noreferrer">'.$state.'</a>')
+                    ->html()
+                    ->sortable(),
+                TextColumn::make('domain')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('app_name')
+                    ->label('App Name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('customer.company_name')
+                    ->label('Customer')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('subscriptionType.name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('deployed_at')
+                    ->label('Deployed Date')
+                    ->dateTime('Y-m-d H:i:s')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('deployed_version')
+                    ->label('Deployed Version')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('subscriptionType.master_version')
+                    ->label('Newest Version')
+                    ->searchable()
+                    ->sortable(),
+                ToggleColumn::make('panic_button_enabled')
+                    ->label('Panic Button')
+                    ->disabled(fn ($record) => !$record || CustomerSubscriptionResource::isThisAppTypeSubscription($record->subscription_type_id)),
+                TextColumn::make('forge_site_id'),
+                TextColumn::make('env_variables_count')
+                    ->label('Variable Count')
+                    ->counts('envVariables'),
+                TextColumn::make('null_variable_count')
+                    ->label('Null Count')
+            ])
+            ->filters([
+                SelectFilter::make('subscription_type_id')
+                    ->label('Subscription Type')
+                    ->relationship('subscriptionType', 'name')
+                    ->options(SubscriptionType::pluck('name', 'id')),
+            ])
+            ->actions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->headerActions([
+                CreateAction::make(),
+                ExportAction::make('table')->fromTable(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    BulkAction::make('send_ent_to_forge')
+                        ->label('Send ENV To Forge')
+                        ->action(fn (Collection $records) => CustomerSubscriptionResource::sendEnvs($records))
+                        ->requiresConfirmation()
+                        ->deselectRecordsAfterCompletion()
+                        ->color('primary'),
+                ]),
+            ]);
     }
 
-
+    public function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->with('customer', 'subscriptionType')
+            ->with('envVariables')
+            ->withCount('envVariables')
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
 }
