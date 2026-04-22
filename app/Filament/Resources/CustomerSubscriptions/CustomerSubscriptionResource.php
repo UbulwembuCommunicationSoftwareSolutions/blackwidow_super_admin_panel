334cc46f5b47ef7ffddcf8e5695a3d79a978d6a3
Jacquestrdx123 <jacques.trdx@gmail.com>
2026-04-22 12:01:45 +0200

    Seeder

--- app/Filament/Resources/CustomerSubscriptions/CustomerSubscriptionResource.php
+++ app/Filament/Resources/CustomerSubscriptions/CustomerSubscriptionResource.php
@@ -25,6 +25,8 @@ class CustomerSubscriptionResource extends Resource
 
     protected static ?string $slug = 'customer-subscriptions';
 
+    protected static bool $shouldRegisterNavigation = false;
+
     protected static string|UnitEnum|null $navigationGroup = 'Customers';
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
