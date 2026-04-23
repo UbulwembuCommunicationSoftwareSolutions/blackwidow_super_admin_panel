d973747429c15933cc95844f2ebd59a409fef21d
Jacques Tredoux <jacques@ncloud.africa>
2026-04-23 13:52:19 +0200

    Seederush

--- app/Models/DeploymentTemplate.php
+++ app/Models/DeploymentTemplate.php
@@ -7,8 +7,15 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DeploymentTemplate extends Model
 {
+
+    public $fillable = [
+        'script',
+        'subscription_type_id',
+    ];
     public function subscriptionType(): BelongsTo
     {
         return $this->belongsTo(SubscriptionType::class);
     }
+
+
 }
