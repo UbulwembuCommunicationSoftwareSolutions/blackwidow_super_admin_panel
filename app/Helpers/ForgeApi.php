d1fa7b24a1c9db7772e5a2a04360e78592edb12c
Jacques Tredoux <jacques@ncloud.africa>
2026-05-08 12:56:04 +0200

    Updates

--- app/Helpers/ForgeApi.php
+++ app/Helpers/ForgeApi.php
@@ -290,9 +290,6 @@ class ForgeApi
                 'project_type' => $customerSubscription->subscriptionType->project_type,
                 'directory' => $customerSubscription->subscriptionType->public_dir,
                 'php_version' => 'php83',
-                'repository' => $customerSubscription->subscriptionType->github_repo,
-                'repository_provider' => 'github',
-                'repository_branch' => $customerSubscription->subscriptionType->branch,
                 'database' => $databaseName,
             ];
         } else {
